1531e9279647ef669903a4939e9febdb154c40d9
Philip Querimit <philip@Philips-MacBook-Air.local>
2020-04-16 18:44:42 -0600

    add files

---
+++ src/components/ToDo.vue
@@ -0,0 +1,25 @@
+<template>
+<li v-if="!todo.completed">
+    <input type="checkbox" v-on:click="completeTodo( todo )" >
+  {{ todo.task }}
+</li>
+</template>
+
+<script>
+  export default {
+      name: 'Todo',
+      props: [
+          'todo'
+      ],
+      methods: {
+          completeTodo ( todo ) {
+            this.$emit( 'complete-todo', todo );
+          }
+      }
+  }
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,52 @@
+<template>
+  <section>
+    <h2>To-Do List</h2>
+    <ul>
+   <Todo v-for="todo in todos" :key="todo.task" :todo="todo" 
+   v-on:complete-todo="completeTodo" />
+    </ul>
+  </section>
+</template>
+
+<script>
+import Todo from './ToDo.vue';
+
+export default {
+    name: 'TodoList',
+    components: {
+        Todo
+    },
+    data () {
+      return {
+        todos: [
+            {
+                task: 'Buy Milk',
+                completed: false
+            },
+             {
+                task: 'Pick up Prescription',
+                completed: true
+            },
+             {
+                task: 'Study Vue',
+                completed: false
+            },
+             {
+                task: 'Install Visual Studio Code',
+                completed: false
+            },
+        ]
+      }
+    },
+    methods: {
+       completeTodo ( todo ) {
+           const todoIndex = this.todos.indexOf( todo );
+           this.todos[todoIndex].completed = true ;
+       }
+    }
+}
+</script>
+   
+<style>
+
+</style>
